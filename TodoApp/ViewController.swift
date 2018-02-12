3d960076bac25e5cde013041e4e64dbeb2f970e8
Ravi Tripathi <ravitripathi1996@gmail.com>
2018-02-12 21:55:29 +0530

    Initial Commit

---
+++ TodoApp/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  TodoApp
+//
+//  Created by Ravi Tripathi on 12/02/18.
+//  Copyright © 2018 Ravi Tripathi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    let itemArray = ["Test","fire"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    //MARK: - TableView Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+    //MARK: -  TableView Delegate
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row])
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        
+    }
+    
+    
+    
+
+}
+
